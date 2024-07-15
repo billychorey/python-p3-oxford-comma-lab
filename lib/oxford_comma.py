b35df2753311934c84a55d20609b7ab481253b81
Billy <billychorey@gmail.com>
2024-07-15 13:51:25 -0400

    oxford comma lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,13 @@
-def oxford_comma(items):
-    return None
+def oxford_comma(items):  
+    if len(items) == 1:
+        return(''.join(items))
+    elif len(items) == 2:
+        return(' and '.join(items))
+    elif len(items) >= 3:
+        return(f"{', '.join(items[:-1])}, and {items[-1]}")
+ 
+        
+        
+print(oxford_comma(["kiwi"]))
+print(oxford_comma(["kiwi", "durian"]))
+print(oxford_comma(['kiwi', 'durian', 'starfruit']))
